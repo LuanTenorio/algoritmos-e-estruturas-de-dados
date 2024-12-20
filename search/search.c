ce12a3d92c1847794964a529ed82b56c77123342
Luan Tenório <lluantenorio7@gmail.com>
2024-12-19 23:19:23 -0300

    Add binary search

--- search/search.c
+++ search/search.c
@@ -10,10 +10,51 @@ int linearSearch(Array *arr, int element){
     return -1;
 }
 
+int binarySearch(Array *arr, int element){
+    int left = 0;
+    int right = arr->length-1;
+    int middle;
+
+    while(right >= left){
+        middle = (right - left) / 2 + left;
+        int middleElement = arr->data[middle];
+     
+        if(element == middleElement)
+            return middle;
+        else if(element > middleElement)
+            left = middle + 1;
+        else
+            right = middle - 1;
+    }
+
+    return -1;
+}
+
+int recursiveBinarySearch(Array *arr, int element, int left, int right){
+    int middle = (right - left) /2 + left;
+    int middleElement = arr->data[middle];
+     
+    if(element == middleElement)
+        return middle;
+    
+    if(left == right)
+        return -1;
+    
+    if(element > middleElement)
+        return recursiveBinarySearch(arr, element, middle + 1, right);
+    else
+        return recursiveBinarySearch(arr, element, left, middle -1);
+}
+
 int main(){
-    int arrData[6] = {1, 2, 3, 4, 5, 6};
-    Array arr = {arrData, 6};
+    int arrData[7] = {1, 2, 3, 4, 5, 6, 7};
+    Array arr = {arrData, 7};
     showArray(arr.data, arr.length);
-    printf("The position of 3 is %d\n", linearSearch(&arr, 3));
+
+    for (size_t i = 1; i <= 7; i++){
+        printf("The position of %ld is %d\n", i, recursiveBinarySearch(&arr, i, 0, arr.length-1));
+        printf("The position of %ld is %d\n", i, binarySearch(&arr, i));
+    }
+    
     return 0;
 }
